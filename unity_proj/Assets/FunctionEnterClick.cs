2cc09f744ee9cb3df0c3205147cb8e345c8d8a1c
Vignesh Gopal <vignesh.gopal011@gmail.com>
2016-04-02 15:21:03 -0400

    funtion string entry

---
+++ unity_proj/Assets/FunctionEnterClick.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunctionEnterClick : MonoBehaviour {
+	public string stringToEdit = "enter your function";
+	void OnGUI() {
+		stringToEdit = GUI.TextField(new Rect(65, 3, 500, 55), stringToEdit);
+	}
+}
