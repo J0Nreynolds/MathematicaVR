a7d22bd2aa90ec7636ade035cbaad91772bac25f
Vignesh Gopal <vignesh.gopal011@gmail.com>
2016-04-03 00:11:34 -0400

    finsihed funtion qwery command

---
+++ unity_proj/Assets/ExampleClass.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	void Update() {
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+}
--- unity_proj/Assets/FunctionEnterClick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class FunctionEnterClick : MonoBehaviour {
-	public string stringToEdit = "enter your function";
-	void OnGUI() {
-		stringToEdit = GUI.TextField(new Rect(65, 3, 500, 55), stringToEdit);
-	}
-}
